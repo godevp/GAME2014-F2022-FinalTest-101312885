7a270ef615f3cb2c5fe70e20e6f7baef0d9fdc6a
godevp <73740267+godevp@users.noreply.github.com>
2022-12-16 17:14:17 -0500

    build

--- Assets/[Scripts]/Gem/Gem.cs
+++ Assets/[Scripts]/Gem/Gem.cs
@@ -5,15 +5,6 @@ using UnityEngine.Rendering;
 
 public class Gem : MonoBehaviour
 {
-    /*private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
-        {
-            collision.gameObject.GetComponent<PlayerBehaviour>().Score++;
-            Destroy(this.gameObject);
-        }
-    }*/
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
